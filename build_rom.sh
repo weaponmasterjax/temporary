f73de5f034c631b83276782e1b4ce3fa05db2653
stuepz <129644009+stuepz@users.noreply.github.com>
2023-04-10 23:17:38 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece --depth 1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b pex .repo/local_manifests
+git clone https://github.com/stuepz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Chongqing #put before last build command
-export BUILD_USERNAME=weaponmasterjax
-export BUILD_HOSTNAME=jaxone
-lunch aosp_everpal-userdebug
+export BUILD_USERNAME=stuepz
+export BUILD_HOSTNAME=woomy
+lunch aosp_viva-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
